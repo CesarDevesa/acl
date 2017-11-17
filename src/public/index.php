3d4a80bf1c24a685eb95dc2ffcc4846493c40904
CesarDevesa <devesa.cesar@gmail.com>
2017-11-17 06:57:21 -0300

    Create 'index.php', update info in '.todo'

---
+++ src/public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+require_once 'vendor\autoload.php';
+
+$permission = new \CodeEdu\Acl\Entities\Permission();
+$permission->setName("view");
+
+$role = new \CodeEdu\Acl\Entities\Role("supervisor");
+$role->addPermission($permission);
+
+//$user = new
+
+$roles[] = $role;
+
+$resource = new \CodeEdu\Acl\Entities\Resource(Book::class, "getUserId");
+$resources[] = $resource;
+
+
+$book = new Book(); ///////
+//book->setName
+
+$acl = new \CodeEdu\Acl\Acl($roles, $resources);
+$acl->isOwner($book, $user);
+*/
